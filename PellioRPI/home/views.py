55a31c4e4b9fc7cc907207194d430104ca0bd835
Mario Nunes <mnunesp2@gmail.com>
2017-11-03 19:08:15 -0400

    Login System complete. CSS working.

--- PellioRPI/home/views.py
+++ PellioRPI/home/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 def index(request):
-    return render(request, 'home/home.html')
+    print("Home page")
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST['username'],password = request.POST['password'])
+            auth_login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'home/home.html', {'form': form})
