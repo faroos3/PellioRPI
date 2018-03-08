c1896d856909cd51b3ecce2453543b8115789fee
LoganNo <lnozell@gmail.com>
2018-03-08 17:08:27 -0500

    Updated home so that a succesful log in
    redirects to a different home page.

--- PellioRPI/home/views.py
+++ PellioRPI/home/views.py
@@ -4,13 +4,16 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate
 
 def index(request):
-    print("Home page")
-    if request.method == 'POST':
-        form = AuthenticationForm(data = request.POST)
-        if form.is_valid():
-            user = authenticate(username=request.POST['username'],password = request.POST['password'])
-            auth_login(request, user)
-            return redirect('index')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'home/home.html', {'form': form})
+	print("Home page")
+	if request.method == 'POST':
+		form = AuthenticationForm(data = request.POST)
+		if form.is_valid():
+			user = authenticate(username=request.POST['username'],password = request.POST['password'])
+			if user is not None:
+				auth_login(request, user)
+				return render(request, 'home/dash.html')
+			else:
+				return render(request, 'home/dash-nouser.html')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'home/home.html', {'form': form})
